3167da5befef7b2c15d87697621a48cae9b67051
Ahmed Youssof <ahmedyoussof92@gmail.com>
2022-06-20 20:52:22 +0200

    create spring boot project

---
+++ src/main/java/com/springudemycourse/spring5quotesapp/Spring5QuotesAppApplication.java
@@ -0,0 +1,13 @@
+package com.springudemycourse.spring5quotesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5QuotesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5QuotesAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springudemycourse/spring5quotesapp/Spring5QuotesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springudemycourse.spring5quotesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5QuotesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
